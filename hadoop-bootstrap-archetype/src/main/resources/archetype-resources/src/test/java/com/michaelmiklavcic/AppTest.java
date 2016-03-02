019e847d52177025d6442eedc21e493b3647f4d4
Michael Miklavcic <mmiklavcic@hortonworks.com>
2016-03-02 14:22:24 -0500

    Hadoop project bootstrap archetype

---
+++ hadoop-bootstrap-archetype/src/main/resources/archetype-resources/src/test/java/com/michaelmiklavcic/AppTest.java
@@ -0,0 +1,12 @@
+package com.michaelmiklavcic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testHello() throws Exception {
+        fail("Not yet implemented");
+    }
+}
